78f4dc4b6db3d5a40870388a8d29f6b107c39408
SpatialCutter <spatialcutter@gmail.com>
2025-04-07 22:54:34 +0600

    tasks_solved

--- src/components/FirstTask.tsx
+++ src/components/FirstTask.tsx
@@ -12,7 +12,15 @@ interface User {
 // Тип, исключающий email
 
 // --- место для решения ---
+type NewUser = Omit<User, "id"> ;
+type ReadonlyUser = Readonly<User>;
+type UserEmailless = Omit<User, "email"> ;
 
+declare global {
+  interface Array<T>  {
+    FilterNumbers(): number[];
+  }
+}
 // ---
 
 function FirstTask() {
@@ -21,7 +29,14 @@ function FirstTask() {
   // console.log([1, 'a', 3, null, 5].filterNumbers(),'test'); // [1, 3, 5]
 
   // --- место для решения ---
+  
 
+  Array.prototype.FilterNumbers = function() {
+    return this.filter(n => (typeof(n) == "number"));
+  }
+
+  console.log([1, 'a', 3, null, 5]);
+  console.log([1, 'a', 3, null, 5].FilterNumbers());
   // ---
 
   // Шаблонные строки
@@ -29,16 +44,39 @@ function FirstTask() {
   // "Hello, Alice! Today is 12.10.2023"
 
   // --- место для решения ---
-
+  console.log(greeting('Alice'));
   // ---
 
   // Проверка объекта на пустоту
 
   // --- место для решения ---
+  let emptyvar;
+  console.log("infact var is undefined");
+  CheckVar(emptyvar);
+  emptyvar = null;
+  console.log("infact var is null");
+  CheckVar(emptyvar);
+  emptyvar = 1;
+  console.log("infact var is 1");
+  CheckVar(emptyvar);
+
+  //Случай объекта
 
   // ---
 
   return <p className="read-the-docs">Смотри консоль</p>;
+}
+
+function CheckVar(variable: any): void {
+  if (variable === undefined)
+    console.log("variable is undefined");
+  else if (variable == null)
+    console.log("variable is null");
+  else console.log("variable is not empty");
+}
+
+function greeting (name : string) : string {
+  return `Hello, ${name}! Today is ${new Date(Date.now()).toLocaleDateString()}`;
 }
 
 export default FirstTask;
--- src/components/SecondTask.tsx
+++ src/components/SecondTask.tsx
@@ -11,8 +11,126 @@
 // в окне с мобом должна быть кнопка удалить с соответствующим функционалом
 // функцию добавления и удаления описать тут, а потом протипизировав передать его в ваш компонент
 
+import { Component, MouseEventHandler, useState } from 'react';
+import './../assets/minecraft.css';
+
+type Mob = {
+  name : string,
+  image : string,
+  id : number,
+  nickname : string
+};
+
+function MobLine(prop: {mob: Mob, onClick: () => void }) {
+
+
+  return <li onClick={prop.onClick}>
+    <p>{prop.mob.id}) {prop.mob.name} [ {prop.mob.nickname} ] </p>
+  </li>
+}
+
+function MobList(prop: {list : Array<Mob>, onMobClick: (mob: Mob) => void}) {
+  return <ul className='line'>
+    {prop.list.map(n => <MobLine key={n.id} mob={n} onClick={() => prop.onMobClick(n)}/>)}
+  </ul>
+}
+
+function DetailMob(prop : {mob: Mob, removeclick: MouseEventHandler, cancelclick: MouseEventHandler}) {
+
+  return <div className='window'>
+    <form  className='windowform'>
+      <p>Детали моба</p>
+      <img src={prop.mob.image} style={{ width: 100, height: 100}}/>
+      <p>ID: {prop.mob.id}</p>
+      <p>name: {prop.mob.name}</p>
+      <p>nickname: {prop.mob.nickname}</p>
+      <button onClick={prop.removeclick}>Удалить</button>
+      <button onClick={prop.cancelclick}>Отмена</button>
+    </form>
+  </div> 
+}
+
+function CreateMob(prop : {savemob: (mob: Mob) => void, cancelclick: MouseEventHandler}) {
+  const [id, setId] = useState<number>(0);
+  const [name, setName] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+  const [image, setImage] = useState<string>('');
+
+  const okclick: MouseEventHandler = (e) => {
+    e.preventDefault();
+    const mob: Mob = { name, image, id, nickname }
+    prop.savemob(mob);
+  }
+
+  return <div className='window'>
+    <form  className='windowform'>
+      <p>Создать нового моба</p>
+      <label>ID:</label>
+      <input type="number" value={id} onChange={(e) => setId(Number(e.target.value))} />
+      <label>name:</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <label>nickname:</label>
+      <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+      <label>image:</label>
+      <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+      <button onClick={okclick}>ОК</button>
+      <button onClick={prop.cancelclick}>Отмена</button>
+    </form>
+  </div> 
+}
+
+function Minecraft(prop: {executer : string}) {
+  const [isNew, setIsNew] = useState(false);
+  const [isLook, setIsLook] = useState(false);
+  const [selectedMob, setSelectedMob] = useState<Mob | null>(null);
+  const [moblist, setMoblist] = useState<Array<Mob>>([]);
+
+  const showwindow: MouseEventHandler = (e) => { 
+    e.preventDefault, 
+    setIsNew(!isNew) 
+  };
+
+  const getmob: (newMob: Mob) => void = (newMob: Mob) => {
+    setMoblist([...moblist, newMob]); // Добавляем нового моба в список
+    setIsNew(false); // Закрываем окно создания моба
+  };
+
+  const showdetail: (newMob: Mob) => void = (mob: Mob) => {
+    setSelectedMob(mob);
+    setIsLook(!isLook);
+  };
+
+  const allcancel: MouseEventHandler = (e) => { 
+    e.preventDefault, 
+    setIsNew(false);
+    setIsLook(false); 
+  };
+
+  const removemob: MouseEventHandler = (e) => {
+    e.preventDefault; 
+    if (selectedMob) {
+      setMoblist(moblist.filter(m => m.id !== selectedMob.id));
+      setIsLook(false);
+      setSelectedMob(null);
+    }
+  };
+
+  return <div>
+    <p>{prop.executer}</p>
+    {
+      isNew ? (<CreateMob savemob={getmob} cancelclick={allcancel}/>) : <p></p>
+    }
+    {
+      (isLook && selectedMob != null) ? (<DetailMob mob={selectedMob} removeclick={removemob} cancelclick={allcancel}/>) : <p></p>
+    }
+
+    <MobList list={moblist} onMobClick={showdetail}/>    
+    <button onClick={showwindow}>Добавить</button>
+  </div>
+}
+
 function SecondTask() {
-  return <p className="read-the-docs">Второе задание ошибки в консоле</p>;
+  return <Minecraft executer="Spatial"/>;
 }
 
 export default SecondTask;
